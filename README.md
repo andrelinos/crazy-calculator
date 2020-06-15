60d48fd2f28f758d5268bc72316282a54ff1b416
Andrelino Silva <andrelinocom@gmail.com>
2020-06-14 22:59:29 -0300

    Criando projeto

---
+++ README.md
@@ -0,0 +1,7 @@
+# crazy-calculator
+<img src='./crazy-calculator.png'>
+
+## Calculadora desenvolvida em HTML, CSS, JavaScript
+Para executar, basta baixar e abrir o arquivo `index.html`
+
+Para editar, basta editar os arquivos usando qualquer editor de texto. 
---
+++ js/main.js
@@ -0,0 +1,33 @@
+function insert(num) {
+    document.form.textview.value = document.form.textview.value + num;
+}
+
+function equal() {
+    var exp = document.form.textview.value;
+    if (exp) {
+        document.form.textview.value = eval(exp);
+    }
+}
+
+function back() {
+    var exp = document.form.textview.value;
+    if (exp) {
+        document.form.textview.value = exp.substring(0, exp.length - 1);
+    }
+}
+
+function cl() {
+    document.form.textview.value = '';
+}
+
+function squareRoot() {
+    var exp = document.form.textview.value;
+    var a = 1,
+        b = 0;
+    for (var i = 0; i < exp; i++) {
+        b = exp / a;
+        a = (b + a) / 2;
+    }
+
+    document.form.textview.value = a.toFixed(11);
+}
